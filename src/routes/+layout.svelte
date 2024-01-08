2d45b8d2b65316bd8a71c616b713cc2939727f9c
dhruvil-codes <22ecdhr063@ldce.ac.in>
2024-01-08 18:15:11 +0530

    chore: add theme

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,12 @@
 <script>
+	import '@fontsource-variable/inter';
 	import '../app.pcss';
 </script>
 
 <slot />
+
+<style>
+	:global(body) {
+		font-family: 'Open Sans Variable', sans-serif;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1 class="font-bold text-slate-600">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { Button } from '@/components/ui/button';
+</script>
+
+<Button>Button</Button>
